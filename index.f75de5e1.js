3746b33f23bcbb2d996aebee52778aee28792689
Ihor Barban <ihorbarban@Ihors-MacBook-Air.local>
2025-01-25 13:41:54 +0100

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,33 @@
+"use strict";
+const food = {
+    Drink: {
+        Wine: {},
+        Schnaps: {}
+    },
+    Fruit: {
+        Red: {
+            Cherry: {},
+            Strawberry: {}
+        },
+        Yellow: {
+            Banana: {},
+            Pineapple: {}
+        }
+    }
+};
+const tree = document.querySelector("#tree");
+function createTree(element, data) {
+    if (Object.keys(data).length > 0) for (const [key, value] of Object.entries(data)){
+        const keyElem = document.createElement("li");
+        keyElem.textContent = key;
+        element.appendChild(keyElem);
+        if (Object.keys(value).length > 0) {
+            const listElem = document.createElement("ul");
+            keyElem.appendChild(listElem);
+            createTree(listElem, value);
+        }
+    }
+}
+createTree(tree, food);
+
+//# sourceMappingURL=index.f75de5e1.js.map
